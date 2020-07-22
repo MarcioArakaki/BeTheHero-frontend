d8b73f7b1ae26297b21c3413d394e682fa5a599e
Marcio Arakaki <marcio_arakaki@outlook.com>
2020-07-22 20:12:57 -0300

    website Register and login calling the web api

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
+import api from '../../services/api'
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+    
+    const [id,setId] = useState('');
+    const history = useHistory();
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.get(`ong/${id}`); 
+            console.log(response);
+            localStorage.setItem("ongId", response.data.id);
+            localStorage.setItem("ongName", response.data.name);
+
+            history.push("/profile");
+        } catch (error)
+        {
+            console.log(error);
+            alert('login failed');
+        }                   
+    }
+
+
     return (
-        <div className="logon-container">
-            <section className="form">
+        <div  className="logon-container">
+            <section onSubmit={handleLogin}  className="form">
                 <img src={logoImg} alt="Be The Hero" className="src" />
                 <form>
                     <h1>Logon</h1>
-                    <input placeholder="Your ID" />
+                    <input value={id} onChange={e => setId(e.target.value)} placeholder="Your ID" />
                     <button className="button" type="submit">Enter</button>
                     <Link className="back-link" to ="/register">
                         <FiLogIn size={16} color="#E02041" />
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
-import {Link} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 
 
 export default function Register() {
+
+    // One state for each field of the form
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [contact, setContact] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            contact,
+            city,
+            uf
+        }
+
+        console.log(data);
+
+        try {
+            const response = await api.post('ong', data);
+            console.log(response);
+            alert(`Your access ID is ${response.data.id}`);
+            
+            history.push('/'); 
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -21,17 +56,17 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholde="ONG Name"/>
-                    <input type="email" placeholde="E-mail"/>
-                    <input placeholde="Phone"/>
+                <form onSubmit={handleRegister}>
+                    <input value={name} onChange={e => setName(e.target.value)} placeholder="ONG Name" />
+                    <input type={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" />
+                    <input type={contact} onChange={e => setContact(e.target.value)} placeholder="Phone" />
 
                     <div className="input-group">
-                        <input placeholder="Cidade"/>
-                        <input placeholder="UF" style={{width: 80}}/>
+                        <input type={city} onChange={e => setCity(e.target.value)} placeholder="Cidade" />
+                        <input type={uf} onChange={e => setUf(e.target.value)} placeholder="UF" style={{ width: 80 }} />
                     </div>
-                     
-                     <button className="button" type="submit">Register</button>
+
+                    <button className="button" type="submit">Register</button>
                 </form>
             </div>
         </div>
