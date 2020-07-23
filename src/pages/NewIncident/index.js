8cc9011ee3dbbe164480b1156f8bd33679a9b223
Marcio Arakaki <marcio_arakaki@outlook.com>
2020-07-22 21:15:13 -0300

    New incident implemented

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api'
 
 
 export default function NewIncident() {
+
+    const[title,setTitle] = useState('');
+    const[description,setDescription] = useState('');
+    const[value,setValue] = useState('');
+    const data = {
+        title,
+        description,
+        value: parseInt(value),
+        ongId: parseInt(localStorage["ongId"])
+    }
+    const history = useHistory();
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        console.log(data);
+        try {
+            const response = await api.post('incident', data);
+            console.log(response);            
+            history.push('/'); 
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -21,10 +48,10 @@ export default function NewIncident() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Incident title" />
-                    <textarea placeholder="Description" />
-                    <input placeholder="Value in dollars " />
+                <form onSubmit={handleNewIncident}>
+                    <input value = {title} onChange = {e => setTitle(e.target.value)} placholder="Incident title" />
+                    <textarea value = {description} onChange = {e => setDescription(e.target.value)} placeholder="Description" />
+                    <input value = {value} onChange = {e => setValue(e.target.value)} placeholder="Value in dollars " />
                     <button className="button" type="submit">Register</button>
                 </form>
             </div>
