d46718a1eb7ed38ca857db9b69ec3c8ddcac72b8
Marcio Arakaki <marcio_arakaki@outlook.com>
2020-07-27 20:59:14 -0300

    Profile pages, fetching lsit from API working

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,84 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
+import api from "../../services/api"
 
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState('');
+    async function handleProfile() {
+
+        try {
+            const response = await api.get(`incident/index/${localStorage.getItem("ongId")}/1`);
+            console.log(response);
+
+            setIncidents(response.data);
+
+
+        } catch (error) {
+            console.log(error);
+            alert('login failed');
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" className="src" />
-                <span>Bem vinda, ONGZERA</span>
+                <span onClick={handleProfile}>Bem vinda, ONGZERA</span>
 
-                <Link className="button" to="/incidents/new">                    
+                <Link className="button" to="/incidents/new">
                     Register new Incident
                 </Link>
                 <button type="button">
                     <FiPower size={18} color="#E02041"></FiPower>
                 </button>
             </header>
-
             <h1>Incidents</h1>
             <ul>
-                <li>
-                    <strong>Caso:</strong>
-                    <p>Incident test</p>
+            {incidents && incidents.map((incident, index) => {
+                return (
+                    <li key={index}>
+                        <strong>Caso:</strong>
+                        <p>{incident.title}</p>
 
-                    <strong>Descripton</strong>
-                    <p>Test description</p>
+                        <strong>Descripton</strong>
+                        <p>{incident.description}</p>
 
-                    <strong>VALUE:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALUE:</strong>
+                        <p>{incident.value}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>Caso:</strong>
-                    <p>Incident test</p>
-
-                    <strong>Descripton</strong>
-                    <p>Test description</p>
-
-                    <strong>VALUE:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>Caso:</strong>
-                    <p>Incident test</p>
-
-                    <strong>Descripton</strong>
-                    <p>Test description</p>
-
-                    <strong>VALUE:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>Caso:</strong>
-                    <p>Incident test</p>
-
-                    <strong>Descripton</strong>
-                    <p>Test description</p>
-
-                    <strong>VALUE:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiTrash2 size={20}  color="#a8a8b3"></FiTrash2>
+                        </button>
+                    </li>
+                )
+            })}
             </ul>
-        </div>);
+        </div >);
 }
